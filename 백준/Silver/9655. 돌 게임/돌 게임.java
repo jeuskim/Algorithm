fd9c99931ddcee5e90745ce16a65071352cc94c3
jeuskim <165736497+jeuskim@users.noreply.github.com>
2024-04-26 18:13:45 +0900

    [Silver V] Title: 돌 게임, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+	/*
+	static long factorial_numerator(long m, long n) {
+		if (n == 0) {
+			return 1;
+		}
+		else if (n == 1) {
+			return m;
+		}
+		else {
+			return m * factorial_numerator(m - 1, n - 1);
+		}
+	}
+	
+	static long factorial_denominator(long n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		else {
+			return n * factorial_denominator(n - 1);
+		}
+	}
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		if (N % 2 == 0) {
+			System.out.println("CY");
+		}
+		else {
+			System.out.println("SK");
+		}
+		/*
+		for (int i = 0; i < T; i++) {
+			N = scanner.nextLong();
+			M = scanner.nextLong();			
+			if (M / 2 < N) {
+				N = M - N;
+			}
+			System.out.println((factorial_numerator(M, N) / factorial_denominator(N)));
+		}
+		*/
+		scanner.close();
+
+		return;
+	}	
+
+}
